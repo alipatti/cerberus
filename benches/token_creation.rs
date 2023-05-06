35d8bb81cc13aa6883732565aed9f85bfd02bae8
Alistair Pattison <alistairpattison@gmail.com>
2023-05-06 14:33:26 -0500

    refactor rng code

--- benches/token_creation.rs
+++ benches/token_creation.rs
@@ -1,11 +1,10 @@
 use array_init::array_init;
 use criterion::{criterion_group, criterion_main, Criterion};
-use rand::{thread_rng, Rng};
 use std::{thread, time::Duration};
 
 fn bench(c: &mut Criterion) {
     // create rng and async runtime
-    let mut rng = thread_rng();
+    let mut rng = rand::thread_rng();
     let tokio_runtime = tokio::runtime::Builder::new_current_thread()
         .build()
         .unwrap();
@@ -17,7 +16,7 @@ fn bench(c: &mut Criterion) {
         .unwrap();
 
     c.bench_function("iter", move |b| {
-        let user_ids = array_init(|_| cerberus::UserId(rng.gen()));
+        let user_ids = array_init(|_| cerberus::UserId::random(&mut rng));
         b.to_async(&tokio_runtime).iter(|| async {
             coordinator.create_tokens(&user_ids).await.unwrap()
         })
